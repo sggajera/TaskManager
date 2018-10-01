886decf38ddb31791e4d084e4b5197976294ecd7
sggajera <39348859+sggajera@users.noreply.github.com>
2018-09-30 22:11:17 -0500

    2ndCommit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports = {mongoURI:'mongodb://<sanket.gajera>:<University2016!>@ds163781.mlab.com:63781/videojot_prod'}
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {mongoURI:'mongodb://sanket.gajera:University2016!@ds163781.mlab.com:63781/videojot_prod'}
 }else{
     module.exports={mongoURI:'mongodb://localhost/vidjot-dev'}
 }
